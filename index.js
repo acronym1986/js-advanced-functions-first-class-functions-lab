d9c6c9fbb1994cad2e3ccb8be0368d96f2c29962
Tim <acronym1986@gmail.com>
2021-03-22 01:45:46 +1100

    code created and passed test

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+//test 1
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+
+//test 2
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(-2);
+}
+
+//test3 4 and 5
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ];
+
+
+//test 6 and 7
+const  createFareMultiplier = function (number){
+    return function(fare) {
+        return parseInt(fare * number);
+    }
+}
+ 
+//test 7 and 8 
+const fareDoubler = function (createFareMultiplier){
+     return createFareMultiplier * 2;
+} 
+
+//test 8 and 9
+const fareTripler = function (createFareMultiplier) {
+     return createFareMultiplier * 3;
+}
+
+
+//test 10 and 11
+const selectDifferentDrivers = function (drivers,selectingDrivers) {
+      return selectingDrivers(drivers) ;
+}
